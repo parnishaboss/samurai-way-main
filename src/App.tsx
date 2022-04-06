4225a45a2e91426fc7a6c5a174f35adea8d17226
parnishaboss <zhora0@mail.ru>
2022-04-06 15:39:49 +0300

    starting build

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+export const App = () => {
+    return (
+        <div className="App-wrapper">
+            <header className="header">
+                <img src="https://art-polevoy.ru/images/portfolio/L2.jpg" alt="logo"/>
+            </header>
+            <nav className="nav">
+                <div><a href="">Profile</a></div>
+                <div><a href="">Messages</a></div>
+                <div><a href="">Music</a></div>
+                <div><a href="">Settings</a></div>
+            </nav>
+            <div className="content">
+                <img src="https://art-oboi.by/assets/images/by_users/Modern-city/adobestock_214402701.jpg" alt=""/>
+                <div>
+                    ava i opisanie
+                </div>
+                <div>
+                    my posts
+                </div>
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
-export default App;
